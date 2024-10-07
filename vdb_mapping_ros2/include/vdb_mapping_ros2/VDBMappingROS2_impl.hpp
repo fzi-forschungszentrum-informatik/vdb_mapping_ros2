60ab1d06dd04b5a8f4dae81fb13d6b827c33735f
Max Schik <schik@fzi.de>
2024-10-07 18:49:58 +0000

    sets qos for cloud subscriber to best effort

--- vdb_mapping_ros2/include/vdb_mapping_ros2/VDBMappingROS2_impl.hpp
+++ vdb_mapping_ros2/include/vdb_mapping_ros2/VDBMappingROS2_impl.hpp
@@ -209,7 +209,7 @@ VDBMappingROS2<VDBMappingT>::VDBMappingROS2(const rclcpp::NodeOptions& options)
 
       m_cloud_subs.push_back(this->create_subscription<sensor_msgs::msg::PointCloud2>(
         sensor_source.topic,
-        1,
+        rclcpp::QoS(1).durability_volatile().best_effort(),
         [&, sensor_source](const std::shared_ptr<sensor_msgs::msg::PointCloud2> cloud_msg) {
           cloudCallback(cloud_msg, sensor_source);
         }));
